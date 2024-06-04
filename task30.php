507428dac400d6a6b0dbab68f3c71600b68fc9da
Sahil100111 <160855019+Sahil100111@users.noreply.github.com>
2024-06-04 23:55:04 +0500

    Add files via upload

---
+++ task30.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis voluptate quia consequuntur tenetur ullam asperiores itaque nobis corporis corrupti voluptates vel, deserunt repellat iure minima reprehenderit eligendi, excepturi beatae perspiciatis.</p>
+    <button id="button">Click me!</button>
+</body>
+</html>
